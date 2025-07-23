5fc84830e8b21e1a3f212949499a85ed0ce333a4
Sultan <134627134+hamzasharif1199@users.noreply.github.com>
2025-07-23 16:08:37 +0500

    step 2 passed
    Allow the add method to handle any amount of numbers.

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -2,7 +2,10 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    return int.parse(numbers);
+    return numbers
+        .split(',')
+        .map((s) => int.tryParse(s.trim()) ?? 0) // fallback to 0 for invalid input
+        .fold(0, (a, b) => a + b);
   }
 
 }
