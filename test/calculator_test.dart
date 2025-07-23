2794b2300ac25db172515c259779d57320b2502c
Sultan <134627134+hamzasharif1199@users.noreply.github.com>
2025-07-23 15:57:14 +0500

    empty string with one number fails

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -2,7 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_calculator/calculator.dart';
 
 void main() {
+
   group('StringCalculator - TDD Step 1', () {
+    test('should return the number when one number is passed', () {
+      final calculator = StringCalculator();
+      expect(calculator.add("1"), 1);
+    });
     test('Empty string should return 0', () {
       final calculator = StringCalculator();
       expect(calculator.add(''), 0);
