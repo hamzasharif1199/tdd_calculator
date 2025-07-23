61a432072c11684cd7ea2cf968cc8223d467faac
Sultan <134627134+hamzasharif1199@users.noreply.github.com>
2025-07-23 14:14:23 +0500

    initial commit
    project clone from github

---
+++ android/app/src/main/kotlin/com/incubyte/tdd_calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.incubyte.tdd_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main_tdd/string_calculator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StringCalculator extends StatelessWidget {
+  const StringCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
